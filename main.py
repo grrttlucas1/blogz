bd534f3523c69330c89ec3158faa692f49803fbf
Garrett Lucas <grrtt_lucas@yahoo.com>
2017-02-23 13:29:51 -0600

    Added login.html and added links to base.html for login/out

--- main.py
+++ main.py
@@ -260,10 +260,12 @@ class LoginHandler(BlogHandler):
 
     # TODO - The login code here is mostly set up for you, but there isn't a template to log in
 
-    def render_login_form(self, error=""):
+    def render_login_form(self, error="",username="",password=""):
         """ Render the login form with or without an error, based on parameters """
         t = jinja_env.get_template("login.html")
-        response = t.render(error=error)
+        response = t.render(error=error,
+                            username=username,
+                            password=password)
         self.response.out.write(response)
 
     def get(self):
@@ -277,12 +279,14 @@ class LoginHandler(BlogHandler):
         user = self.get_user_by_name(submitted_username)
 
         if not user:
-            self.render_login_form(error="Invalid username")
+            self.render_login_form(error="Invalid username",
+                                   username=submitted_username)
         elif hashutils.valid_pw(submitted_username, submitted_password, user.pw_hash):
             self.login_user(user)
             self.redirect('/blog/newpost')
         else:
-            self.render_login_form(error="Invalid password")
+            self.render_login_form(error="Invalid password",
+                                   username=submitted_username)
 
 class LogoutHandler(BlogHandler):
 
